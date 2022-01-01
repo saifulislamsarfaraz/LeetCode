5da93acc05b9ba4746216bbcc8e6e8ea84bb4d4d
Saiful Islam Sarfaraz <saifulislamsarfaraz@gmail.com>
2022-01-01 11:18:44 +0600

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        complementMap = dict()
+        
+        for i in range(len(nums)):
+            num = nums[i]
+            complement = target - num
+            if num in complementMap:
+                return [complementMap[num], i]
+            else:
+                complementMap[complement] = i
+           
